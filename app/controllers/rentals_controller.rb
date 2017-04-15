cf4938f0251f27596accfe1d0c55ba497b3850f8
Jun <x16123107@student.ncirl.ie>
2017-04-15 23:52:02 +0000

    set url to Rentals and Reviews for admin as '/allrentals' and '/allreviews'

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -111,6 +111,10 @@ class RentalsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def allrentals
+    @rentals = Rental.all
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resources :rentals
   
   get 'signedinuserprofile' => 'profiles#signedinuserprofile'
+  get 'allreviews' => 'reviews#allreviews'
+  get 'allrentals' => 'rentals#allrentals'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
